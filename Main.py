1dd7e8dc055797f852801f55cb29fdcfe5c3ac0c
NafeeJ <nafeejan2@gmail.com>
2020-07-20 19:41:09 -0400

    Implemented basic multithreading

--- Main.py
+++ Main.py
@@ -5,47 +5,34 @@ import threading
 
 class Civ:
 
-    def __init__(self, item):
-        self.item = 0
+    def __init__(self, numOfGoods, nameOfGoods):
+        self.numOfGoods = numOfGoods
+        self.nameOfGoods = nameOfGoods
 
-    @property
-    def produce(self):
-        self.item += 1
-
-def background():
-    wood = 0
-    berries = 0
-    c1 = Civ(wood)
-    c2 = Civ(berries)
-
-
+    def produce(self, productionOutput, productionInterval):
+        self.numOfGoods += productionOutput
+        print("Number of " + self.nameOfGoods + ": " , self.numOfGoods)
+        time.sleep(productionInterval)
 
 
 def main():
     print("Start")
-    """wood = 0
+    wood = 0
     berries = 0
-    c1 = Civ(wood)
-    c2 = Civ(berries)"""
+    c1 = Civ(wood, "wood")
+    c2 = Civ(berries, "berries")
 
     try:
         while True:
-            c1.produce
-            c2.produce
 
-            print(c1.item)
-            print(c2.item)
+            c1Thread = threading.Thread(target=c1.produce(1, 2), args=(1, 2))
+            c2Thread = threading.Thread(target=c2.produce(2, 3), args=(2, 3))
 
-            time.sleep(3)
+            c1Thread.start()
+            c2Thread.start()
+
     except KeyboardInterrupt: #press CTRL+C
         pass
-
-
-"""        c1.produce
-        c2.produce
-
-        print(c1.item)
-        print(c2.item)"""
 
 if __name__ == '__main__':
     main()
