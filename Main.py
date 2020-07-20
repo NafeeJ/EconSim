5cef1217a51f51852e9047c68a974099058c2868
NafeeJ <nafeejan2@gmail.com>
2020-07-20 15:37:38 -0400

    Some stuff for Andy

---
+++ Main.py
@@ -0,0 +1,20 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import sched, time
+
+#Society 1
+class CivOne() :
+    wood = 0
+
+
+
+
+def main():
+    plt.plot([1, 2, 3, 4])
+    plt.ylabel('some numbers')
+    plt.show()
+
+if __name__ == '__main__':
+    main()
+
+def
