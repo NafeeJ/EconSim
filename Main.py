9d24f08f20c498a71247192d2bd59d5bdd6fc123
AndrewRednikov <59496249+AndrewR0@users.noreply.github.com>
2020-07-20 16:18:39 -0400

    Basics of Civ

--- Main.py
+++ Main.py
@@ -2,19 +2,34 @@ import numpy as np
 import matplotlib.pyplot as plt
 import sched, time
 
-#Society 1
-class CivOne() :
-    wood = 0
+class Civ:
 
+    def __init__(self, item):
+        self.item = 0
 
-
+    @property
+    def produce(self):
+        self.item += 1
 
 def main():
-    plt.plot([1, 2, 3, 4])
-    plt.ylabel('some numbers')
-    plt.show()
+    wood = 0
+    berries = 0
+    c1 = Civ(wood)
+    c2 = Civ(berries)
+
+    c1.produce
+    c2.produce
+
+    print(c1.item)
+    print(c2.item)
+
+    time.sleep(3)
+
+    c1.produce
+    c2.produce
+
+    print(c1.item)
+    print(c2.item)
 
 if __name__ == '__main__':
     main()
-
-def
